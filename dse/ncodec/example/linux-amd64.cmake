18afb21e6db7cab7e2697a182111fb6f42c8f538
Rogoz Mateusz (VM/EMT3) <Mateusz.Rogoz@pl.bosch.com>
2025-04-02 17:20:29 +0200

    win os example

---
+++ dse/ncodec/example/linux-amd64.cmake
@@ -0,0 +1,11 @@
+# Target: Linux AMD64
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CDEF_PLATFORM_OS linux)         # Available via dse/clib/platform.h.
+set(CDEF_PLATFORM_ARCH amd64)       # Available via dse/clib/platform.h.
+
+
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+endif()
